6fe520c1e4227aab28a5879c779bea10168dc102
FengYu <17674036561@163.com>
2024-05-05 22:06:05 +0800

    version 0.0.1 第一次提交了代码 first code

---
+++ process.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "process.h"
+using namespace std;
+
+void Process::planProcess(){
+    cout<<"this is planing process"<<endl;
+    my_map.mapInfo();
+    cout<<"planing success!"<<endl;
+}
---
+++ process.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "pnc_map.h"
+
+class Process
+{
+public: //子函数
+    void planProcess();
+
+public: //成员变量
+    PNC_Map my_map;
+};
